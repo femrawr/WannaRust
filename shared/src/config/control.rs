75532459297d63d98221223221002f5f466a9b28
femrawr <femrawr0@gmail.com>
2025-05-25 21:25:59 +0800

    crypto address

--- shared/src/config/control.rs
+++ shared/src/config/control.rs
@@ -7,5 +7,5 @@ pub const TTS_MESSAGE: &'static str = "This device has been raped by the wanna r
 /// whether the wannarust/utils/files hide_item function can work
 pub const CAN_HIDE_ITEMS: bool = false;
 
-/// the crypto address in the ransome not
+/// the crypto address in the ransome note
 pub const CRYPTO_ADDRESS: &'static str = "LZb1q9nfsRgLxG1Bz7FVJZTeXVKjHxfX2U";
